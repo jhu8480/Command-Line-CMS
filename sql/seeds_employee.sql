3ce39e2217afcf38ee3da195cfacdc69e4f3baf2
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-02-18 14:54:16 -0500

    seeded db

---
+++ sql/seeds_employee.sql
@@ -0,0 +1,10 @@
+INSERT INTO employee (employee_id, first_name, last_name, role_id, manager_id) VALUES
+  (1002, 'Yuequn', 'Jiang', 201, 1001),
+  (1003, 'Yiwei', 'Tian', 202, 1002),
+  (1004, 'Yanan', 'Zhu', 203, 1001),
+  (1005, 'Haoqing', 'Hu', 204, 1004),
+  (1006, 'Yao', 'Rao', 205, 1001),
+  (1007, 'Yichen', 'Mei', 206, 1006),
+  (1008, 'John', 'Doe', 207, 1001);
+
+SELECT * FROM employee;
---
+++ sql/seeds_roles.sql
@@ -0,0 +1,10 @@
+INSERT INTO roles(title, salary, department_id) VALUES
+  ('Accountant', 4.50, 101),
+  ('Teller', 3.50, 101),
+  ('Sales Manager', 5.00, 102),
+  ('Sales Specialist', 3.50, 102),
+  ('Tooling Technician', 7.50, 103),
+  ('General Labor', 2.50, 103),
+  ('Truck Driver', 6.80, 104);
+
+SELECT * FROM roles;
