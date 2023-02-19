edb6a8ee731fc21e68f82319cc7e81744f2316f4
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-02-19 16:12:53 -0500

    modified viewAll function

--- index.js
+++ index.js
@@ -45,6 +45,9 @@ function initCMS() {
       else if (todo === 'Add a Department') {
         addDepartment();
       }
+      else if (todo === 'Add a Role') {
+        addRole();
+      }
       else if (todo === 'Quit') {
         console.log('\n\n\n---------------------Bye bye 👋 See you soon!---------------\n\n\n');
         process.exit(1);
@@ -54,17 +57,74 @@ function initCMS() {
 
 async function viewAll(table) {
   try {
-    const queryString = `SELECT * FROM ${table}`;
+    let queryString;
+    switch (table) {
+      case 'department':
+        queryString = 'SELECT * FROM department';
+        break;
+      case 'roles':
+        queryString = 'SELECT roles.role_id as id, roles.title, department.department_id, department.department_name, roles.salary FROM roles JOIN department ON roles.department_id = department.department_id';
+        break;
+      case 'employee':
+        queryString = `SELECT e.employee_id AS id, e.first_name, e.last_name, roles.title, department.department_name AS department, roles.salary, CONCAT(m.first_name, ' ', m.last_name) as manager FROM employee e JOIN roles ON e.role_id = roles.role_id JOIN department ON roles.department_id = department.department_id JOIN employee m ON e.manager_id = m.employee_id`;
+        break;
+    }
     const data = await db.query(queryString);
     console.log(`\n\n-----------------------\n\n`);
     console.log(cTable.getTable(data[0]));
   } catch(e) {
     console.error(e);
+    process.exit(1);
   } finally {
     initCMS();
   }
 }
 
 async function addDepartment() {
-  
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Please enter name of the new department',
+      name: 'department'
+    }
+  ]).then(
+    async ({department}) => {
+    try {
+      const queryString = `INSERT INTO department(department_name) VALUES(?)`;
+      const response = await db.query(queryString, department);
+      console.log(response);
+    } catch(e) {
+      console.error(e);
+      process.exit(1);
+    } finally {
+      initCMS();
+    }
+  })
+}
+
+async function addRole() {
+  const getAllDepartments = 'SELECT department_name FROM department';
+  const departmentsObjArr = await db.query(getAllDepartments);
+  const allDepartments = [];
+  for (let i = 0; i < departmentsObjArr[0].length; i++) {
+    allDepartments.push(departmentsObjArr[0][i].department_name);
+  }
+
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is the name of the new role?',
+      name: 'role'
+    },
+    {
+      type: 'input',
+      message: 'What is the salary of the new role?',
+      name: 'salary'
+    }, {
+      type: 'list',
+      message: 'Please pick the department the new role belongs to',
+      name: 'department'
+    }
+  ])
+
 }
---
+++ sql/query.sql
@@ -0,0 +1,3 @@
+SELECT roles.role_id as id, roles.title, department.department_id, department.department_name, roles.salary FROM roles JOIN department ON roles.department_id = department.department_id;
+
+SELECT e.employee_id AS id, e.first_name, e.last_name, roles.title, department.department_name AS department, roles.salary, CONCAT(m.first_name, ' ', m.last_name) as manager FROM employee e JOIN roles ON e.role_id = roles.role_id JOIN department ON roles.department_id = department.department_id JOIN employee m ON e.manager_id = m.employee_id;
